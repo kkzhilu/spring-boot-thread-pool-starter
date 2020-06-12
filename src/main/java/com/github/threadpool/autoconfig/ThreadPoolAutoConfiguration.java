17d61ca7ce2a5c5ef295d432f07ac28afd19eaf5
柯贤铭 <kexianming@testin.cn>
2020-06-12 09:26:54 +0800

    update

--- src/main/java/com/github/threadpool/autoconfig/ThreadPoolAutoConfiguration.java
+++ src/main/java/com/github/threadpool/autoconfig/ThreadPoolAutoConfiguration.java
@@ -99,5 +99,5 @@ public class ThreadPoolAutoConfiguration {
         return threadPoolExecutor;
     }
 
-        private static final ThreadFactory DEFAULT_THREAD_FACTORY = Thread::new;
+    private static final ThreadFactory DEFAULT_THREAD_FACTORY = Thread::new;
 }
